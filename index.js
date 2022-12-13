48f38e464dd9cc816edb7c8763e133c165e159f9
Kostya Kan <kostikkan595@gmail.com>
2022-12-13 17:18:01 +0500

    feat: Added more http methods

--- index.js
+++ index.js
@@ -50,48 +50,56 @@ app.post('/regUser', async (request, response) => {
       name: request.body.name,
       email: request.body.email,
       password: passHash,
+      avatarImgPath: '',
     };
     const result = await users.insertOne(query);
     return result
       ? response.status(200).json({ success: true })
       : response.status(404).json({ success: false, message: 'Unknown error' });
   } else {
-    return response
-      .status(400)
-      .json({
-        success: false,
-        message: isNameTaken ? 'Name already taken' : 'Email already taken',
-      });
+    return response.status(400).json({
+      success: false,
+      message: isNameTaken ? 'Name already taken' : 'Email already taken',
+    });
   }
 });
 app.patch('/logUser', async (request, response) => {
   const query = {
-    email: request.body.email
-  }
-  const result = await users.findOne(query)
-  const isPassValid = bcrypt.compareSync(request.body.password,result.password)
-  return isPassValid ? response.status(200).json({success: true, message: 'Welcome back!'}) : response.status(401).json({success: false, message: 'Invalid password.'})
+    email: request.body.email,
+  };
+  const result = await users.findOne(query);
+  const isPassValid = bcrypt.compareSync(
+    request.body.password,
+    result.password
+  );
+  return isPassValid
+    ? response.status(200).json({ success: true, message: 'Welcome back!' })
+    : response
+        .status(401)
+        .json({ success: false, message: 'Invalid password.' });
 });
 
 ////////////////////////USERS///////////////////////
 ////////////////////////REVIEWS///////////////////////
 
 app.get('/getAllReviews', async (request, response) => {
-  const result = await reviews.find();
+  const result = await reviews.find().toArray();
   if (result) {
-    return response.status(200).json({ success: true });
+    return response.status(200).json({ success: true, reviews: result });
   }
 });
 
-app.post('/createReview', async (request, response) => {
+app.post('/insertReview', async (request, response) => {
   const query = {
     title: request.body.title,
     descr: request.body.descr,
-    groups: request.body.groups,
+    group: request.body.group,
     tags: request.body.tags,
-    author: request.body.author,
+    imgPath: request.body.imgPath,
+    // author: request.body.author,
     likes: 0,
     dislikes: 0,
+    views: 0,
     rating: request.body.rating,
     comments: [],
   };
@@ -101,21 +109,40 @@ app.post('/createReview', async (request, response) => {
   }
 });
 
+app.post('/updateViewsReview', async (request, response) => {
+  const query = {
+    _id: new ObjectId(request.body._id),
+  };
+  const targetReview = await reviews.findOne(query);
+  const updateQuery = {
+    $set: {
+      views: targetReview.views + 1,
+    },
+  };
+  const updateResult = await reviews.updateOne(query, updateQuery);
+  console.log(updateResult,'update result');
+  if (updateResult) {
+    return response.status(200).json({ success: true });
+  }
+});
+
 app.patch('/editReview', async (request, response) => {
-   const query = {
-    _id: new ObjectId(request.body._id)
-   }
-   const editQuery = {
-    $set:{
+  const query = {
+    _id: new ObjectId(request.body._id),
+  };
+  const editQuery = {
+    $set: {
       title: request.body.title,
       descr: request.body.descr,
       groups: request.body.groups,
       tags: request.body.tags,
       rating: request.body.rating,
-    }
-   }
-   const result = await reviews.findOneAndUpdate(query,editQuery)
-   return result ? response.status(200).json({success: true})
+    },
+  };
+  const result = await reviews.findOneAndUpdate(query, editQuery);
+  return result
+    ? response.status(200).json({ success: true })
+    : response.status(404).json({ success: false, message: 'Unknown error' });
 });
 
 ////////////////////////REVIEWS///////////////////////
