b58e580178da1a68d2976d2fd1e5825b86f38938
Kostya Kan <kostikkan595@gmail.com>
2023-01-06 23:07:35 +0500

    fix: Added client origin

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
