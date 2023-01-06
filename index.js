e343ddb6de78619e4b00184a5573ab2a403e60d8
Kostya Kan <kostikkan595@gmail.com>
2023-01-07 01:15:34 +0500

    fix: Changed cors origin

--- index.js
+++ index.js
@@ -8,27 +8,11 @@ dotenv.config();
 app.use(
   cors({
     credentials: true,
-    origin: '*',
+    origin: 'https://frontview-kang.netlify.app',
     optionSuccessStatus: 200,
   })
 );
-app.use(function (req, res, next) {
 
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  next();
-});
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
