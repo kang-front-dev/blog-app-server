c3a943b6d7c534a29b05b7fe98b9c40dd2b7d95e
Kostya Kan <kostikkan595@gmail.com>
2023-01-06 19:16:55 +0500

    fix: Changed static client url

--- index.js
+++ index.js
@@ -2,15 +2,10 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const dotenv = require('dotenv');
-const cookieParser = require('cookie-parser');
+
 dotenv.config();
 
-app.use(
-  cors({
-    credentials: true,
-    origin: process.env.CLIENT_URL,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
