f8b42d4ab0cfda09da75a65c139f88a30561b758
Kostya Kan <kostikkan595@gmail.com>
2023-01-07 01:50:22 +0500

    fix: Added back allowedHeaders in cors config

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-app.use(cors());
+app.use(cors({
+  allowedHeaders: ['Content-type','Authorization'],
+  methods: ['GET','POST','PATCH','DELETE','PUT'],
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
